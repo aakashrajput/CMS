a3f4fc99ee654b6027895adffcb16b377a0e11a3
aakashrajput <aakashsingh1999@gmail.com>
2018-11-05 21:35:25 +0530

    minor changes

---
+++ include/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>CMS</title>
+  <link rel="icon" type="image/png" href="assets/img/favicon-black.png">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Acme|Anton|Charmonman|Lobster|Pacifico|Srisakdi" rel="stylesheet">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/animate.css">
+  <link rel="stylesheet" href="assets/css/fontawesome.css">
+  <link rel="stylesheet" href="assets/css/fontawesome.min.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.css">
+</head>
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-black">
+        <a class="navbar-brand" href="#"><img src="assets/img/icon.png" alt="logo" width="70px">Poké</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.html"><i class="fas fa-home"></i> Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-file"></i> Pages</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Page 1</a>
+              <a class="dropdown-item" href="#">Page 2</a>
+            </div>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-list-ul"></i> Categories</a>
+            <div class="dropdown-menu" aria-labelledby="dropdown01">
+              <a class="dropdown-item" href="#">Category 1</a>
+              <a class="dropdown-item" href="#">Category 2</a>
+            </div>
+          </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.html"><i class="fas fa-phone"></i> Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    <div class="jumbotron">
+      <div class="container">
+        <div id="details" class="animated fadeInLeft">
+          <h1 style="font-family: 'Pacifico', cursive;">Poké <span>Blog</span></h1>
+          <p style="font-family: 'Pacifico', cursive;">Learn wth us all about Pokémon!</p>
+        </div>
+      </div>
+      <img src="http://dualmonitorswallpaper.com/wp-content/uploads/2018/02/multi-monitor-wallpaper-pokemon.png" alt="heading image">
+    </div>
